c1a5a579dfa2b190b5f5a18a5dece21b15ffd2c8
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2019-06-09 13:49:05 +0200

    first commit

---
+++ TestFirstWFapp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFirstWFapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(button1.BackColor != Color.Green)
+            {
+                button1.BackColor = Color.Green;
+                button1.Text = "Green!";
+            } else
+            {
+                button1.BackColor = Color.Red;
+                button1.Text = "Red!";
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(this);
+            form2.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+        }
+    }
+}
---
+++ TestFirstWFapp/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFirstWFapp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(Form1 f)
+        {
+            InitializeComponent();
+            f.BackColor = Color.Pink;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Button newButon = new Button();
+            newButon.BackColor = Color.BlanchedAlmond;
+            newButon.ForeColor = Color.Black;
+            newButon.Text = "Hello Button";
+            newButon.Location = new Point(10, 10);
+            this.Controls.Add(newButon);
+        }
+    }
+}
